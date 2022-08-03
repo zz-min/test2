eb568625e7276971b0ea61149148a393ca439781
zz-min <kjm77878@gmail.com>
2022-08-03 21:47:14 +0900

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by kjmjimin on 2022/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
